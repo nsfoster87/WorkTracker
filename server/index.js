993454d417d54e628685abcff01b922271fa838f
Nate Foster <nate.s.foster@gmail.com>
2023-07-02 19:42:32 -0400

    create db pool

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const pool = require('../db/index.js');
 
 app.use(express.static('./client/dist'));
+
+app.post('/signup', addNewUser); // addNewUser not yet implemented
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
