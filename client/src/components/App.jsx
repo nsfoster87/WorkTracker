655db4fec27ff084ebf1562545191d7064a82c15
Nate Foster <nate.s.foster@gmail.com>
2023-07-02 14:02:28 -0400

    create signup form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Signup from './Signup.jsx';
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Signup />
+    </>
   );
 }
