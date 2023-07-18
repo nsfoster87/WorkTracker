228ed0c7bb6cf62a3aef084056bd4a49f73234b0
Nate Foster <nate.s.foster@gmail.com>
2023-07-18 13:27:54 -0400

    userModel createUser and findByUsername

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const pool = require('../db/index.js');
 app.use(express.static('./client/dist'));
 
 // app.post('/signup', addNewUser); // addNewUser not yet implemented
+// app.post('/login', loginUser);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ server/models/userModel.js
@@ -0,0 +1,35 @@
+const pool = require('../../db/index.js');
+
+const createUser = async ({ username, password, salt }) => {
+  const query = 'INSERT INTO users (username, password, salt) VALUES ($1, $2, $3)';
+  const values = [username, password, salt];
+
+  const client = await pool.connect();
+  try {
+    const result = await client.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.log(`Error inserting new user ${username} into db`, error);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+const findByUsername = async (username) => {
+  const query = 'SELECT * FROM users WHERE username = $1';
+  const values = [username];
+
+  const client = await pool.connect();
+  try {
+    const result = await client.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.log(`Error fetching user ${username} from db`, error);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+module.exports = { createUser, findByUsername };
