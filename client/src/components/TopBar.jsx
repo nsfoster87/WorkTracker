462004b2f1e0b245f7bb9f079c46cd42c4eb37d9
Nate Foster <nate@Nates-MacBook-Air.local>
2023-07-21 17:55:33 -0400

    implement topBar (not hooked up to backend yet, only state

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Box } from '@mui/material';
+import { 
+  AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Box,
+  Dialog, DialogTitle, DialogContent, TextField, Button
+} from '@mui/material';
 import { Add } from '@mui/icons-material';
 
 const TopBar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [jobs, setJobs] = useState(['Job 1', 'Job 2']);
+  const [jobs, setJobs] = useState(['Test Job 1', 'Test Job 2']);
   const [selectedJob, setSelectedJob] = useState(jobs[0]);
+  const [newJobName, setNewJobName] = useState('');
+  const [isAddJobDialogOpen, setAddJobDialogOpen] = useState(false);
 
   const handleMenuOpen = (e) => {
     setAnchorEl(e.currentTarget);
@@ -15,43 +20,89 @@ const TopBar = () => {
     setAnchorEl(null);
   };
 
-  const handleAddJob = () => {
-    console.log('adding new job');
-    // setJobs([...jobs, newJob]);
-  }
-
-  const handleJobChange = (job) => {
+  const handleJobClick = (job) => {
     setSelectedJob(job);
     setAnchorEl(null);
+  };
+
+  const handleAddJobClick = () => {
+    setAddJobDialogOpen(true);
+    setAnchorEl(null);
+  };
+
+  const handleAddJobDialogClose = () => {
+    setAddJobDialogOpen(false);
+    setNewJobName('');
+  }
+
+  const handleAddNewJob = () => {
+    if (newJobName.trim() !== '') {
+      const newJob = newJobName.trim();
+      setJobs([...jobs, newJob]);
+      setSelectedJob(newJob);
+    }
+    handleAddJobDialogClose();
   }
 
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Typography variant="h6" align="center" onClick={handleMenuOpen} sx={{ cursor: 'pointer' }}>
-            {selectedJob}
-          </Typography>
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            {jobs.map(job => (
-              <MenuItem key={job} onClick={() => handleJobChange(job)}>
-                {job}
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" align="center" onClick={handleMenuOpen} sx={{ cursor: 'pointer' }}>
+              {selectedJob}
+            </Typography>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+            >
+              {jobs.map(job => (
+                <MenuItem key={job} onClick={() => handleJobClick(job)}>
+                  {job}
+                </MenuItem>
+              ))}
+              <MenuItem onClick={handleAddJobClick}>
+                <IconButton>
+                  <Add />
+                </IconButton>
+                Add New Job
               </MenuItem>
-            ))}
-            <MenuItem onClick={handleAddJob}>
-              <IconButton color="inherit" onClick={handleAddJob}>
-                <Add />
-              </IconButton>
-              Add Job
-            </MenuItem>
-          </Menu>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Dialog open={isAddJobDialogOpen} onClose={handleAddJobDialogClose}>
+        <DialogTitle>Add New Job</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Job Name"
+            fullWidth
+            value={newJobName}
+            onChange={(e) => setNewJobName(e.target.value)}
+          />
+        </DialogContent>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', padding: '8px' }}>
+          <Button onClick={handleAddJobDialogClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleAddNewJob} color="primary">
+            Add
+          </Button>
         </Box>
-      </Toolbar>
-    </AppBar>
+      </Dialog>
+    </>
   );
 }
 
